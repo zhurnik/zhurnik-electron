dfd82ff49907c39e7b49fff9ad9ba2514eaa2443
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2017-01-23 04:30:06 +1300

    Add DateInput component

---
+++ src/components/DateInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DateInput = (props) => {
+  return (
+    <form onSubmit= { props.changeDate } >
+      <input type='text'
+             className='date_input'
+             value={props.displayDate}
+      />
+      <input type='submit' style={{display: 'none'}} />
+    </form>
+  );
+}
+
+export default DateInput;
